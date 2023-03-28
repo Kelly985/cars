62395c5b7fbd7925d3da70ed826292fa9d30e503
MJephat <mukundijephat22@gmail.com>
2023-03-28 11:28:34 +0300

    fetched and displayed

--- app.js
+++ app.js
@@ -2,5 +2,24 @@
 fetch("http://localhost:3000/cars")
         .then(res => res.json())
         .then(data =>{
+              for(let i=0; i<data.length; i++){
+                console.log(data[i]);
+
+            let Model = data[i].model;
+            let Make = data[i].make;
+            let Year = data[i].year;
+            let Price = data[i].price;
+            let Pic = data[i].image_url;
+
+            document.querySelector(".container").innerHTML+=`
+            <image src="${Pic}">
+            <p>${Year}</p>
+            <p>${Price}</p<
+            <p>${Make}</p>
+            <P>${Model}}
+        }
+            `
+
+            }
         
         })
